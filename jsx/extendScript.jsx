dec5b78b06bbbeb83c9d20cadfaceda33be9a99b
gerkx <ptgerke@gmail.com>
2018-09-07 13:59:14 +0200

    locates markers, imports grfx - need to integrate mod of props

--- jsx/extendScript.jsx
+++ jsx/extendScript.jsx
@@ -1,9 +1,49 @@
-// $.runScript = {
-//     changeThisFunctionName: function() {
-//         alert("Boop");
-//     }	
-// }
+function createOverlays() {
+    const seq = app.project.activeSequence;
+    const markers = markersToArray(seq.markers);
 
-function booper() {
-    alert("double boop")
+
+    var filterString = "";
+    if (Folder.fs === 'Windows'){
+        filterString = "Motion Graphics Templates:*.mogrt";
+    }
+    var mogrtToImport = File.openDialog (
+        "Choose MoGRT", // title
+        filterString,  // filter available files? 
+        false
+    );	
+    
+    var targetTime = markers[0].start;
+    var vidTrackOffset = 0;
+    var audTrackOffset = 0;
+    var newTrackItem = seq.importMGT(	
+        mogrtToImport.fsName, 
+        targetTime.ticks, 
+        vidTrackOffset,
+        audTrackOffset
+    );
+
+    var moComp = newTrackItem.getMGTComponent();
+    var params = moComp.properties;
+    var srcTextParam = params.getParamForDisplayName("Source Text");
+    var val	= srcTextParam.getValue();
+    srcTextParam.setValue("New value set by PProPanel!");
+    
+
+}
+
+
+function markersToArray(markerObject){
+    var markerArr = [];
+    var currMarker;
+    for(var i=0; i<markerObject.numMarkers; i++){        
+        if(i==0){
+            currMarker = markerObject.getFirstMarker();
+            markerArr.push(currMarker);
+        }else{
+            currMarker = markerObject.getNextMarker(currMarker);
+            markerArr.push(currMarker);
+        }
+    }
+    return markerArr;
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 /* 1) Create an instance of CSInterface. */
-var csInterface = new CSInterface();
+const csInterface = new CSInterface();
 
 /* 2) Make a reference to your HTML button and add a click handler. */
-var openButton = document.querySelector("#btn");
+const openButton = document.querySelector(".btn");
 openButton.addEventListener("click", runJSX);
 
 /* 3) Write a helper function to pass instructions to the ExtendScript side. */
 function runJSX() {
-  csInterface.evalScript("booper()");
+  csInterface.evalScript("createOverlays()");
 }
